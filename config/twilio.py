78ace85a21104d24a87701ff6fb7a2fd90de1266
mikeg7895 <90222525+mikeg7895@users.noreply.github.com>
2024-12-17 12:03:26 -0500

    Update twilio.py
    delete the crendentials

--- config/twilio.py
+++ config/twilio.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account_sid = "AC049edd2bf0b498b15c43dbeb8b6a16e7"
-auth_token = "f6c3dc956857d88de76edaf991e9617c"
+account_sid = "************************"
+auth_token = "*************************"
 
 def get_client():
     return Client(account_sid, auth_token)
